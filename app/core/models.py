f31c2842cb6b86bf788fed90d04f5277dead4224
jacksbs <86292108+jacksbs@users.noreply.github.com>
2021-09-22 12:02:17 +0200

    ok

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,6 @@ class UserManager(BaseUserManager):
 
         return user
 
-
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
         user = self.create_user(email, password)
